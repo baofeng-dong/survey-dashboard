b329f1b636ef9463cde8fd45a2a9fb048d72d27d
Dong, Baofeng <dongb@trimet.org>
2016-11-23 15:38:39 -0800

    appended svg chart to surveyors page

--- dashboard/static/js/surveyors.js
+++ dashboard/static/js/surveyors.js
@@ -17,6 +17,15 @@ function build_table(data,tb_id){
     }
 }
 
+//append a svg file based on selected question num to div-id: line-chart
+
+function append_img(div_id, args){
+    var div = $(div_id);
+    console.log(args);
+    var date = args['date'];
+    console.log('date: ' + date)
+    div.empty().prepend('<object data='+ base + 'static/image/' + 'surveyors-' + date + '.svg?' + Math.random() + ' type="image/svg+xml" width="100%" />');
+}
 
     function rebuild_users(url, args) {
         $.getJSON(url, args, function(data) {
@@ -24,12 +33,22 @@ function build_table(data,tb_id){
             var data = data.data;
             console.log(data);
             div_id = "#surveyors";
+            div_id_ln = "#line-chart"
             tb_id = div_id + "-table";
             build_table(data,tb_id);
+            append_img(div_id_ln,args);
+            $(div_id).show();
+            $("#line-chart").show();
         });
     }
  
-    
+
+function reset(){
+    $("#surveyors").hide();
+    $("#line-chart").hide();
+}
+
+// function to pick a date
     function get_date(today) {
         if(!today) today = new Date();
         var dd = today.getDate();
@@ -51,7 +70,7 @@ function build_table(data,tb_id){
     var url = "surveyors/_summary";
 
     $("#date-select").val(get_date());
-    //build table for the selected date
+
     rebuild_users(url, {'date':get_date()});
 
     $("#date-select").datepicker({
@@ -60,6 +79,7 @@ function build_table(data,tb_id){
         onSelect: function(sel_date, dp) {
             console.log("selected date");
             console.log(sel_date);
+            reset();
             rebuild_users(url, {'date':sel_date});
         }
     });
