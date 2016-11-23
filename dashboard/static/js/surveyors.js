a422a32ac5099720251886b7c7cefa6b627f5f7f
Dong, Baofeng <dongb@trimet.org>
2016-11-22 16:33:43 -0800

    added surveyors summary html page and javascript to build table based on returned data from /surveyors/_summary

---
+++ dashboard/static/js/surveyors.js
@@ -0,0 +1,65 @@
+
+//build table using JSON data in jquery
+function build_table(data,tb_id){
+
+    var table = $(tb_id)
+    console.log(table)
+    $(".data-row").empty()
+    for(var i = 0;i < data.length; i++){
+        console.log(data[i]);
+        var row = $("<tr>",{class: "data-row"});
+        for(var j = 0; j < data[i].length; j++){
+            var cell = $("<td>", {align:"center",valign:"middle"}).text(data[i][j]);
+            console.log(data[i][j])
+            row.append(cell)
+        }
+        table.append(row)
+    }
+}
+
+
+    function rebuild_users(url, args) {
+        $.getJSON(url, args, function(data) {
+            console.log(data);
+            var data = data.data;
+            console.log(data);
+            div_id = "#surveyors";
+            tb_id = div_id + "-table";
+            build_table(data,tb_id);
+        });
+    }
+ 
+    
+    function get_date(today) {
+        if(!today) today = new Date();
+        var dd = today.getDate();
+        console.log(dd);
+        var mm = today.getMonth()+1; //January is 0!
+        console.log(mm);
+        var yyyy = today.getFullYear();
+        console.log(yyyy);
+
+        if(dd < 10) {
+            dd='0'+dd
+        } 
+        if(mm < 10) {
+            mm='0'+mm
+        } 
+        return yyyy + '-' + mm + '-' + dd;
+    }
+    
+    var url = "surveyors/_summary";
+
+    $("#date-select").val(get_date());
+    //build table for the selected date
+    rebuild_users(url, {'date':get_date()});
+
+    $("#date-select").datepicker({
+        dateFormat: "yy-mm-dd",
+        autoclose:true,
+        onSelect: function(sel_date, dp) {
+            console.log("selected date");
+            console.log(sel_date);
+            rebuild_users(url, {'date':sel_date});
+        }
+    });
