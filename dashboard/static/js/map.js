d61001bd25e4e358c9768aff754f2e7e1570d3ef
Baofeng Dong <dongb@trimet.org>
2016-11-23 22:06:16 -0800

    added leaflet basemap

---
+++ dashboard/static/js/map.js
@@ -0,0 +1,14 @@
+
+//initialize map 
+
+
+$(document).ready(function() {
+    mymap = L.map('mapid', {scrollWheelZoom:true}).setView([45.48661, -122.65343], 11);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG11c2VyMTk3IiwiYSI6ImNpc254cHk1YTA1dngydm14bjkyamQ1NmsifQ.8ya7T1hHXtVmYOwMrVIuFw', {
+    attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a>',
+    id: 'mapbox.light',
+    maxZoom: 18,
+    minZoom: 11
+    }).addTo(mymap);
+
+})
