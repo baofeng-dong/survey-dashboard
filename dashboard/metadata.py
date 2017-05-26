83054112b86300f65efcd65fd7862253daaa5b1c
Baofeng Dong <DongB@trimet.org>
2017-05-26 01:43:57 -0700

    added metadata file for tracking question num, divs, and headers

---
+++ dashboard/metadata.py
@@ -0,0 +1,71 @@
+metadata = {
+    1: {
+        "headers": ["Satisfaction", "Count", "Pct"],
+        "id": "satisfaction"
+        },
+    2: {
+        "headers": ["Origin", "Count", "Pct"],
+        "id": "origin"
+        },
+    3: {
+        "headers": ["Destination", "Count", "Pct"],
+        "id": "destination"
+        },
+    4: {
+        "headers": ["Travel Change", "Count", "Pct"],
+        "id": "travelchange"
+        },
+    5: {
+        "headers": ["Gasoline Importance", "Count", "Pct"],
+        "id": "gasoline"
+        },        
+    6: {
+        "headers": ["Ridership", "Count", "Pct"],
+        "id": "ridership"
+        },
+    7: {
+        "headers": ["Ride Years", "Count", "Pct"],
+        "id": "rideyears"
+        },
+    8: {
+        "headers": ["Job Approval", "Count", "Pct"],
+        "id": "approval"
+        },
+    9: {
+        "headers": ["One Change", "Count", "Pct"],
+        "id": "onechange"
+        },
+    10: {
+        "headers": ["Age", "Count", "Pct"],
+        "id": "age"
+        },
+    11: {
+        "headers": ["Gender", "Count", "Pct"],
+        "id": "gender"
+        },
+    12: {
+        "headers": ["Household Size", "Count", "Pct"],
+        "id": "house"
+        },
+    13: {
+        "headers": ["Income", "Count", "Pct"],
+        "id": "income"
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
