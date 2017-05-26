6c3db8cc638fb4f5a2d6f59a42925c7eb85a58af
Baofeng Dong <DongB@trimet.org>
2017-05-26 01:43:14 -0700

    added file

---
+++ dashboard/auth.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2016 Baofeng Dong
+# This program is released under the "MIT License".
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
+
+from functools import wraps
+from flask import Response, request
+from dashboard import app, error
+
+class Auth(object):
+    
+    @staticmethod
+    def check_auth(username, password): 
+        return username == app.config["USER_NAME"] and password == app.config["USER_PW"]
+
+    @staticmethod
+    def authenticate():
+        """Sends a 401 response that enables basic auth"""
+        return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+    @staticmethod
+    def requires_auth(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            auth = request.authorization
+            if not auth or not Auth.check_auth(auth.username, auth.password):
+                return Auth.authenticate()
+            return f(*args, **kwargs)
+        return decorated
+
